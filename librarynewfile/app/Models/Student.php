407be200812fa6131844c2859fdc7fb326550f78
melokb2023 <kyle.melo@lccdo.edu.ph>
2023-04-30 21:11:11 +0800

    nowcommiting

---
+++ librarynewfile/app/Models/Student.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $table= 'student';
+    
+    protected $fillable = [
+         'idNo',
+         'firstName',
+         'middleName',
+         'lastName',
+         'suffix',
+         'course',
+         'year',
+         'birthDate',
+         'gender',
+    ];
+}

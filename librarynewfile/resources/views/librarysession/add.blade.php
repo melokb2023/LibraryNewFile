523b1ef5e02f6d2ff45ec9d0c595cb1a0f5faefa
ccaparas666 <carmelo.caparas@lccdo.edu.ph>
2023-06-01 03:12:03 +0800

    Color Update

--- librarynewfile/resources/views/librarysession/add.blade.php
+++ librarynewfile/resources/views/librarysession/add.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-indigo-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-black-900 dark:text-black-100">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
                    <h6 style="text-align:center">Errors Encountered</h6>
                     @if($errors)
                        <ul>
@@ -21,7 +21,7 @@
                         @csrf
                       <div class="flex-items-center" style="text-align:center"><label for="Student Number">Student Number</label>
                      <div>  
-                       <select name="xsno">
+                       <select class="text-black font-bold" style="text-align:center" style="text-align:center" name="xsno">
                             @foreach($student as $stuinfo)
                             <option value="{{$stuinfo->sno }}"> {{$stuinfo->idNo}} - {{$stuinfo->lastName}}, {{$stuinfo->firstName}} {{$stuinfo->middleName}} {{$stuinfo->suffix}}</option>
                             @endforeach
@@ -30,12 +30,12 @@
                 </div>
                        <div class="flex-items-center" style="text-align:center"><label for="Student Purpose">Student Purpose</label>
                     <div>
-                    <input type="text"  name="xstudentpurpose" value="{{old('xstudentpurpose')}}"/>
+                    <input class="text-black font-bold" style="text-align:center"  type="text"  name="xstudentpurpose" value="{{old('xstudentpurpose')}}"/>
                     </div>
 </div>
                     <div class="flex-items-center" style="text-align:center"><label for="Student Session">Student Session</label>
                     <div>
-                    <input type="text"  name="xstudentsession" value="{{old('xstudentsession')}}"/>
+                    <input class="text-black font-bold" style="text-align:center"  type="text"  name="xstudentsession" value="{{old('xstudentsession')}}"/>
                     </div>
 </div>
            
